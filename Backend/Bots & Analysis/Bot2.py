7fbca256acca32b70c80448244fc0234df34b2ea
MozartofCode <bertan.berker@gmail.com>
2024-06-05 06:15:58 -0400

    Made some progress with the NN, experimenting

--- Backend/Bots & Analysis/Bot2.py
+++ Backend/Bots & Analysis/Bot2.py
@@ -1,10 +1,25 @@
-# AI-powered bot (pretrained bot from hugging face...)
+# @Author: Bertan Berker
+# @Filename: Bot2.py
+# Bot2 is an AI bot that makes decisions and bettings based on a neural network and the .csv dataset
+# that I generated with the moves of bot1
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models, Model
+from sklearn.model_selection import train_test_split
+
+
 
 class Bot2:
     def __init__(self, money):
         self.money = money
         self.hand = []
         self.hand2 = []
+
+    #TODO
+    def play(self, input_data):
+        return
 
     
     def hit(self, card):
@@ -16,3 +31,9 @@ class Bot2:
     def calculate_hand_val(self):
         return    
     
+
+
+
+
+
+
