e107460eb8e4c2e5fb26820923c9cff4e48f3c3e
MozartofCode <bertan.berker@gmail.com>
2024-06-05 14:10:17 -0400

    Made progress with integrating NN to bot2 class. Going to work on game

--- Backend/Bots & Analysis/Bot2.py
+++ Backend/Bots & Analysis/Bot2.py
@@ -3,23 +3,41 @@
 # Bot2 is an AI bot that makes decisions and bettings based on a neural network and the .csv dataset
 # that I generated with the moves of bot1
 
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras import layers, models, Model
-from sklearn.model_selection import train_test_split
-
-
+from NN import predict_move, create_NN_model
 
 class Bot2:
     def __init__(self, money):
         self.money = money
         self.hand = []
         self.hand2 = []
+        self.model = create_NN_model()
+
+
+    def play(self, player_hand, house_upcard, count, bet, is_initial):
+        if is_initial:
+            move = predict_move(player_hand, house_upcard, count, bet, self.model)
+            return move
+                
+        # Hits if < 17 as a bot if not initial hand
+        else:
+            if self.calculate_hand_val() < 17:
+                return "H"
+            else:
+                return "S"
 
     #TODO
-    def play(self, input_data):
-        return
+    def play2(self, player_hand, house_upcard, count, bet, is_initial):
+        if is_initial:
+            move = predict_move(player_hand, house_upcard, count, bet, self.model)
+            return move
+                
+        # Hits if < 17 as a bot if not initial hand
+        else:
+            if self.calculate_hand_val2() < 17:
+                return "H"
+            else:
+                return "S"
+
 
     
     def hit(self, card):
