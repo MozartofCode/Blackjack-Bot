f95d10173f1a67f34bdf3dcf7e8674d9463ce33d
MozartofCode <bertan.berker@gmail.com>
2024-06-14 11:29:36 -0400

    Now house's card is finally facedown

--- Frontend/src/components/House.js
+++ Frontend/src/components/House.js
@@ -2,13 +2,24 @@ import React from 'react';
 import Card from './Card';
 import '../styles/house.css'
 
+
 const House = ({ house }) => {
+  
+  const DownCardImage = require(`../down_card.png`);
+  
+
   return (
     <div className="player-container">
       <h2>House</h2>
       <div className="cards-container">
-        {house.cards.map((card, index) => (
-          <Card key={index} card={card} />
+      {house.cards.map((card, index) => (
+          index === 0 ? (
+            <div key={index} className="card">
+              <img src= {DownCardImage} alt="Facedown card" />
+            </div>
+          ) : (
+            <Card key={index} card={card} />
+          )
         ))}
       </div>
       <div className="money">Money: ${house.money}</div>
