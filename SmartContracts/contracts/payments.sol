90d4c3892abea2b0d812ebbbc3ea215b719b9cbe
MozartofCode <bertan.berker@gmail.com>
2024-06-18 09:52:24 -0400

    Wrote a simple smart contract to automate the payments between house and player in the fullstack application

---
+++ SmartContracts/contracts/payments.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+// @Author: Bertan Berker
+// @File: payments.sol
+// This is a smart contract that handles the payments between the player and the house
+
+pragma solidity ^0.8.2;
+
+
+contract Payments {
+
+    uint256 house_balance;
+    uint256 player_balance;
+
+    constructor(uint256 _house, uint256 _player) {
+        house_balance = _house;
+        player_balance = _player;
+
+    }
+
+    function addHouse(uint256 _money) public {
+        house_balance += _money;
+    }
+
+    function addPlayer(uint256 _money) public {
+        player_balance += _money;
+    }
+
+    function subHouse(uint256 _money) public {
+        house_balance -= _money;
+    }
+
+    function subPlayer(uint256 _money) public {
+        player_balance += _money;
+    }
+
+    function getHouseBalance() public view returns (uint256) {
+        return house_balance;
+    }
+
+    function getPlayerBalance() public view returns (uint256) {
+        return player_balance;
+    }
+
+}
